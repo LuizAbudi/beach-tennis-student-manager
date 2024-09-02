b8a83e070ee95147d9b0b05f8145104c489abcdd
AlysonJorge <alysongjorge@gmail.com>
2024-09-02 17:13:19 -0300

    Arrumado o token jwt de professor não enviando o ID do professor

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -67,13 +67,35 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    const payload = {
+    let payload;
+    
+    if (user.userType === 'teacher') {
+      const teacher = await this.teacherRepository.findOne({
+        where: { user: { id: user.id } },
+      });
+      if (!teacher) {
+        throw new NotFoundException
+    } else{
+       payload = {
+        sub: user.id,
+        email: user.email,
+        id: user.id,
+        userType: user.userType,
+        name: user.name,
+        teacherId: teacher.id,
+      };
+    }
+
+  } else{
+     payload = {
       sub: user.id,
       email: user.email,
       id: user.id,
       userType: user.userType,
       name: user.name,
     };
+  }
+
 
     return { access_token: await this.jwtService.signAsync(payload) };
   }
